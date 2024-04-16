2299f18decf9a481a47b2ebcd7fcec89741a8516
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2024-04-16 10:59:17 +0300

    initial commit

---
+++ fishingLuckyBite/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+
+
+const App=()=> {
+  
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 20 }}>Hello World!</Text>
+    </View>
+  );
+}
+
+
+
+export default App;
